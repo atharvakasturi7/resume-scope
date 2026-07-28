1d3ef32f9473484070a81c7e7fd53e5a1e87a5ea
Atharva Kasturi <atharvakasturi@gmail.com>
2026-07-28 20:30:29 +0530

    Use environment port for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -29,8 +29,8 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 const multer = require("multer");
